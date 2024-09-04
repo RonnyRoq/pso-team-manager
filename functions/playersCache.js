219c10cf7b72da3f0b3b1f5950cd89e4ad529c25
liam <liam@liamhardman.com>
2024-09-04 20:31:47 +0100

    FIX - hopefully resolving various bot problems when there is exactly x000 members in the PSAF discord, player support upped from 5k - 10k - UNTESTED

--- functions/playersCache.js
+++ functions/playersCache.js
@@ -10,10 +10,10 @@ export const getAllPlayers = async (guild_id) => {
     while(allPlayers.length === 0) {
       await sleep(5000)
       allPlayers = playersCache.get(guild_id)
-    }  
+    }
     return allPlayers
   }
-  
+
   playersCache.set(guild_id, [])
   let lastId = 0
   let totalPlayers = []
@@ -21,11 +21,13 @@ export const getAllPlayers = async (guild_id) => {
   let i=0
   do{
     const playersResp = await DiscordRequest(`/guilds/${guild_id}/members?limit=1000&after=${lastId}`, { method: 'GET' })
-    currentPlayers= await playersResp.json()
+    currentPlayers = await playersResp.json()
     totalPlayers = totalPlayers.concat(currentPlayers)
     i++
-    lastId = currentPlayers[currentPlayers.length-1].user.id
-  } while (currentPlayers.length === 1000 && i<5) // wont support more than 5000 players.
+    if (currentPlayers.length > 0) {
+      lastId = currentPlayers[currentPlayers.length - 1].user.id
+    }
+  } while (currentPlayers.length === 1000 && i < 10) // Max player count of 10,000
   playersCache.set(guild_id, totalPlayers)
   return totalPlayers
 }
