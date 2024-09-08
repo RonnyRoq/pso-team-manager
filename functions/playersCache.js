55dd3c1871d8eef78fe1adc74e4364aa321905d8
liam <liam@liamhardman.com>
2024-09-08 10:55:58 +0100

    Resolved caching error when there is exactly x000 members in the PSAF discord, upped complete max to 10k members

--- functions/playersCache.js
+++ functions/playersCache.js
@@ -2,12 +2,12 @@ import NodeCache from "node-cache";
 import { DiscordRequest } from "../utils.js";
 import { sleep } from "./helpers.js";
 
-const playersCache = new NodeCache({ stdTTL: 60, checkperiod: 120, useClones: false})
+const playersCache = new NodeCache({ stdTTL: 60, checkperiod: 120, useClones: false })
 
 export const getAllPlayers = async (guild_id) => {
   let allPlayers = playersCache.get(guild_id)
-  if(allPlayers) {
-    while(allPlayers.length === 0) {
+  if (allPlayers) {
+    while (allPlayers.length === 0) {
       await sleep(5000)
       allPlayers = playersCache.get(guild_id)
     }
@@ -16,18 +16,23 @@ export const getAllPlayers = async (guild_id) => {
 
   playersCache.set(guild_id, [])
   let lastId = 0
-  let totalPlayers = []
+  let totalPlayers = []  // Properly initialize totalPlayers as an empty array
   let currentPlayers = []
-  let i=0
-  do{
+  let i = 0
+  do {
     const playersResp = await DiscordRequest(`/guilds/${guild_id}/members?limit=1000&after=${lastId}`, { method: 'GET' })
     currentPlayers = await playersResp.json()
-    totalPlayers = totalPlayers.concat(currentPlayers)
-    i++
-    if (currentPlayers.length > 0) {
-      lastId = currentPlayers[currentPlayers.length - 1].user.id
+    if (Array.isArray(currentPlayers)) {  // Ensure currentPlayers is an array before concatenating
+      totalPlayers = totalPlayers.concat(currentPlayers)
+      i++
+      if (currentPlayers.length > 0) {
+        lastId = currentPlayers[currentPlayers.length - 1].user.id
+      }
+    } else {
+      console.error('Unexpected response format:', currentPlayers)
+      break  // Exit the loop if the response is not an array
     }
-  } while (currentPlayers.length === 1000 && i < 10) // Max player count of 10,000
+  } while (currentPlayers.length === 1000 && i < 10) // Now supports up to 10,000 players
   playersCache.set(guild_id, totalPlayers)
   return totalPlayers
 }
